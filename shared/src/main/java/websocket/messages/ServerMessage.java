d8b237ffa08af0b59868e1a175f1d5a198095306
zenith <alexanderdavidatkinson@gmail.com>
2024-08-06 17:01:34 -0600

    Sever Message type changes for error messages

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -1,16 +1,20 @@
 package websocket.messages;
 
+import chess.ChessGame;
+
 import java.util.Objects;
 
 /**
  * Represents a Message the server can send through a WebSocket
- * 
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
 //Needs to implement 3 main server messages: Load game, error, and notificatoin (see specs for details)
 public class ServerMessage {
     ServerMessageType serverMessageType;
+    private ChessGame game;
+    private String message;
+    private String errorMessage;
 
     public enum ServerMessageType {
         LOAD_GAME,
@@ -18,23 +22,44 @@ public class ServerMessage {
         NOTIFICATION
     }
 
-    public ServerMessage(ServerMessageType type) {
-        this.serverMessageType = type;
+    // Constructor for LOAD_GAME messages
+    public ServerMessage(ChessGame game) {
+        this.serverMessageType = ServerMessageType.LOAD_GAME;
+        this.game = game;
     }
 
     public ServerMessageType getServerMessageType() {
-        return this.serverMessageType;
+        return serverMessageType;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    //error and notifications
+    public ServerMessage(ServerMessageType valtype, String message) {
+        this.serverMessageType = valtype;
+        switch (valtype) {
+            case NOTIFICATION -> this.message = message;
+            case ERROR -> this.errorMessage = message;
+            default -> throw new IllegalArgumentException("Incorrect variable type");
+        }
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
+        if (this == o)
             return true;
-        }
-        if (!(o instanceof ServerMessage)) {
+        if (!(o instanceof ServerMessage that))
             return false;
-        }
-        ServerMessage that = (ServerMessage) o;
         return getServerMessageType() == that.getServerMessageType();
     }
 
@@ -42,7 +67,6 @@ public class ServerMessage {
     public int hashCode() {
         return Objects.hash(getServerMessageType());
     }
+
+
 }
-
-
-
